47593ccfe5fde60de18d5f21747021333eac0971
Okky Stafford <okkystafford@Okkys-MacBook-Pro.local>
2025-05-19 14:14:44 +1000

    Removing and starting again

--- scripts/start-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /Users/okkystafford/Documents/repositories/Boot.dev/docker-configs/databases
-docker-compose up -d
-echo "Database services started!"
-echo "PostgreSQL: localhost:5432 (user: bootdev, password: bootdev, database: bootdevdb)"
-echo "MySQL: localhost:3306 (user: bootdev, password: bootdev, database: bootdevdb)"
-echo "Adminer: http://localhost:8080"
--- scripts/start-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/okkystafford/Documents/repositories/Boot.dev/docker-configs/linux-playground
-docker-compose up -d
-echo "Linux containers started!"
-echo "To access Ubuntu container: docker exec -it bootdev-ubuntu bash"
-echo "To access Alpine container: docker exec -it bootdev-alpine sh"
