a15628625a49b234738f6812392efa92968cc608
Okky <okky.mstafford@gmail.com>
2025-05-24 21:38:35 +1000

    Restart
    Starting over again

--- python/project-template/main.py
+++ python/project-template/main.py
@@ -1,6 +1,7 @@
 def unlock_achievement(before_xp, ach_xp, ach_name):
     return None, None
 
-    # before_xp = int
-    # ach_xp = int
-    # ach_name = str
+# before_xp = int
+# ach_xp = int
+# ach_name = str
+
